357fe4999abc2209b28a5cdc54751b6ca154517b
Lee <jayden.lee@pnnl.gov>
2023-08-14 10:44:53 -0700

    added code to implement a gpt3 generation of a medical report using the saved pneumonia cnn model

---
+++ medical_report.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from PIL import Image
+from io import BytesIO
+import requests
+import openai
+
+# Load the trained Pneumonia Detection model (PneumoniaCNN)
+class PneumoniaCNN(nn.Module):
+    # PneumoniaCNN implementation
+
+# Load the pre-trained Pneumonia Detection model
+pneumonia_model = PneumoniaCNN()
+pneumonia_model.load_state_dict(torch.load("path_to_pneumonia_model.pth"))
+pneumonia_model.eval()
+
+# Set up your GPT-3 API key
+api_key = "YOUR_GPT_3_API_KEY"
+openai.api_key = api_key
+
+# Load and preprocess the chest X-ray image
+image_url = "URL_TO_YOUR_CHEST_XRAY_IMAGE"
+response = requests.get(image_url)
+image = Image.open(BytesIO(response.content))
+# Preprocess the image (e.g., resize, normalize)
+
+# Make a Pneumonia Prediction using the trained model
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+input_image = transform(image).unsqueeze(0)  # Add a batch dimension
+with torch.no_grad():
+    output = pneumonia_model(input_image)
+probabilities = nn.functional.softmax(output, dim=1)
+predicted_class = torch.argmax(probabilities, dim=1)
+pneumonia_prediction = "Pneumonia Positive" if predicted_class.item() == 1 else "Pneumonia Negative"
+
+# Formulate Input Prompt for GPT-3
+input_prompt = f"Patient: A 35-year-old male with a chest X-ray showing {pneumonia_prediction}. Image: {image}\nMedical History: Describe any symptoms, medical history, and other relevant details.\nDiagnosis: Please provide a medical report based on the information provided."
+
+# Request Medical Report from GPT-3
+response = openai.Completion.create(
+    engine="davinci",
+    prompt=input_prompt,
+    temperature=0.7,
+    max_tokens=200
+)
+
+# Process the GPT-3 Output
+generated_report = response.choices[0].text
+# Extract the medical report from the generated_report
+
+# Display the Medical Report
+print("Generated Medical Report:")
+print(generated_report)
